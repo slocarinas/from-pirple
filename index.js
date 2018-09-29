f12103ffe9f443870efef7ad4c48f53a0bcfc7e2
slocarinas <33009351+slocarinas@users.noreply.github.com>
2018-09-29 12:39:20 -0700

    Added basic url parsing

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 
-
+// Dependencies
 var http = require("http");
+var url = require("url");
 
+// The server should reposnd to all requests with a string
 var server = http.createServer(function(req, res){
+
+    // Get the url and parse it
+    var parsedURL = url.parse(req.url, true);
+
+    // Get the path
+    var path = parsedURL.path;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Send the response
     res.end("Hello World!");
+
+    // Log the request path
+    console.log('Request received on path: ' + trimmedPath);
 });
 
 server.listen(3000, function(){
